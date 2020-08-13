b0ce967f5eddcbfb4a1f33d1875671fa14a5e0bc
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-13 06:38:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+require 'pry'
+
+def run_guessing_game
+ num = rand(6)+1
+ input = gets.chomp.to_i
+ 
+ if input == num
+   puts "You guessed the correct number!"
+  elsif input != num
+  puts "Sorry! The computer guessed #{num}."
+  elsif input == "exit"
+  puts "Goodbye!"
+ end
+end
